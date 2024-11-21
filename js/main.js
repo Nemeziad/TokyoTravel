8ea7972637e25c1e1621b7bebe830234e59c13c6
Nemeziad <danilperovskij@MacBook-Air-Danil.local>
2024-11-21 20:48:21 +0300

    modal fix

--- js/main.js
+++ js/main.js
@@ -31,6 +31,7 @@ const openModalBtn = document.getElementById('main__cont_open-btn')
 openModalBtn.addEventListener('click', function () {
 	document.getElementById('main__cont_my_modal').classList.add('open')
 })
+
 const closeModalBtn = document.getElementById(
 	'main__cont_modal_close_my_modal_btn'
 )
--- js/modal.js
+++ js/modal.js
@@ -1,11 +1,9 @@
-document
-	.getElementById('main__cont_open-btn')
-	.addEventListener('click', function () {
-		document.getElementById('main__cont_my_modal').classList.add('open')
-	})
+const openModalBtn = document.getElementById('main__cont_open-btn')
+openModalBtn.addEventListener('click', function () {
+	document.getElementById('main__cont_my_modal').classList.add('open')
+})
 
-document
-	.getElementById('main__cont_modal_close_my_modal_btn')
-	.addEventListener('click', function () {
-		document.getElementById('main__cont_my_modal').classList.remove('open')
-	})
+const closeModalBtn = document.getElementById('main__cont_modal_close_my_modal_btn')
+closeModalBtn.addEventListener('click', function () {
+	document.getElementById('main__cont_my_modal').classList.remove('open')
+})
